283f4fd84f98efc1de2dd5043e341ef86c0394f7
Akshay2002Singh <06akshay2002@gmail.com>
2023-01-27 09:41:50 +0530

    Updating css

---
+++ Pages/Components/footer.js
@@ -0,0 +1,54 @@
+var footer=document.createElement("footer")
+footer.innerHTML =`
+        <div class="social">
+          <p>Follow me on social media</p>
+          <ul class="wrapper">
+            <a href="https://www.github.com" target="_blank">
+              <li class="icon github">
+                <span class="tooltip">GitHub</span>
+                <span><i class="fab fa-github"></i></span>
+              </li>
+            </a>
+            <a href="https://www.instagram.com" target="_blank">
+              <li class="icon instagram">
+                <span class="tooltip">Instagram</span>
+                <span><i class="fab fa-instagram"></i></span>
+              </li>
+            </a>
+            <a href="https://www.codepen.io" target="_blank">
+              <li class="icon github">
+                <span class="tooltip">CodePen</span>
+                <span><i class="fab fa-codepen"></i></span>
+              </li>
+            </a>
+          </ul>
+        </div>
+        <div class="final_text"></div>
+        <p>Rapid Coders</p>
+    `
+document.body.appendChild(footer)
+
+
+
+
+{/* <a href="https://www.facebook.com" target="_blank">
+              <li class="icon facebook">
+                <span class="tooltip">Facebook</span>
+                <span><i class="fab fa-facebook-f"></i></span>
+              </li>
+            </a> 
+
+          <a href="https://www.twitter.com" target="_blank">
+              <li class="icon twitter">
+                <span class="tooltip">Twitter</span>
+                <span><i class="fab fa-twitter"></i></span>
+              </li>
+            </a>
+
+            <a href="https://www.linkedin.com" target="_blank">
+              <li class="icon linkedin">
+                <span class="tooltip">LinkedIn</span>
+                <span><i class="fab fa-linkedin"></i></span>
+              </li>
+            </a>
+          */}
---
+++ Pages/Components/nav.js
@@ -0,0 +1,47 @@
+var footer=document.createElement("nav")
+footer.innerHTML =`<nav>
+<div class="navbar_custom">
+	
+<div class="logo">
+    <h2>Coders</h2>
+</div>
+
+<ul class="links">
+    <li><a href="/index.html">Home</a></li>
+    <li><a href="/Pages/pbooks.html">Programming Books</a></li>
+    <li><a href="/Pages/semester.html">Semester Books</a></li>
+    <li><a href="/Pages/developers.html">Developers</a></li>
+</ul>
+
+
+<div class="toggle">
+    <div class="line1"></div>
+    <div class="line2"></div>
+    <div class="line3"></div>
+</div>
+</div>
+</nav>
+    `
+document.body.appendChild(footer)
+
+
+
+// nav toogle 
+const navbar = document.querySelector('.navbar_custom')
+
+navbar.querySelector('.toggle').addEventListener('click',()=>{
+	
+	navbar.classList.toggle('collapsed')
+	
+})
+
+window.addEventListener('scroll',(e)=>{
+	
+	let windowY = window.pageYOffset
+	
+	let navbarHeight = document.querySelector('.navbar').offsetHeight
+	
+	if(windowY>navbarHeight) navbar.classList.add('sticky')
+	else navbar.classList.remove('sticky')
+	
+})
