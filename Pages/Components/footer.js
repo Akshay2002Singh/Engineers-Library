9f373d54f9e685f44e41a62a2eb44cdb952534aa
Akshay2002Singh <06akshay2002@gmail.com>
2023-01-29 12:47:24 +0530

    adding links

--- Pages/Components/footer.js
+++ Pages/Components/footer.js
@@ -3,19 +3,19 @@ footer.innerHTML =`
         <div class="social">
           <p>Follow me on social media</p>
           <ul class="wrapper">
-            <a href="https://www.github.com" target="_blank">
+            <a href="https://github.com/Akshay2002Singh/" target="_blank">
               <li class="icon github">
                 <span class="tooltip">GitHub</span>
                 <span><i class="fab fa-github"></i></span>
               </li>
             </a>
-            <a href="https://www.instagram.com" target="_blank">
+            <a href="https://www.instagram.com/rapidcoders.in/" target="_blank">
               <li class="icon instagram">
                 <span class="tooltip">Instagram</span>
                 <span><i class="fab fa-instagram"></i></span>
               </li>
             </a>
-            <a href="https://www.codepen.io" target="_blank">
+            <a href="https://codepen.io/akshay-singh-the-decoder/" target="_blank">
               <li class="icon github">
                 <span class="tooltip">CodePen</span>
                 <span><i class="fab fa-codepen"></i></span>
